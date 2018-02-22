be05de986d54edd448a4a83a02c91eacca1ffd0d
Eddie Taliaferro II <eddielacrosse2@gmail.com>
2018-02-22 13:41:29 -0500

    Update artistpage.component.ts

--- src/app/library/artists/artistpage/artistpage.component.ts
+++ src/app/library/artists/artistpage/artistpage.component.ts
@@ -1,28 +1,29 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { ActivatedRoute, Params, Router, ActivatedRouteSnapshot } from '@angular/router';
 import { DataService } from './../../../data.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { trigger, state, style, transition, animate, keyframes} from '@angular/animations';
+
+
 
 @Component({
   selector: 'artistpage',
   templateUrl: './artistpage.component.html',
   styleUrls: ['./artistpage.component.css']
 })
-
 export class ArtistPageComponent implements OnInit{
 
-  artistId= '';
-  artistData = this.dataService.data;
+  artistName: any = null;
 
   constructor(
     private route: ActivatedRoute,
-    private router: Router,
-    private dataService: DataService
+
   ){  }
 
   ngOnInit(){
-     this.artistId = this.route.snapshot.params.artistId;
-     console.log(this.artistId);
+     this.artistName = this.route.snapshot.params['artistName'];
+
   }
 
-
 }
+
