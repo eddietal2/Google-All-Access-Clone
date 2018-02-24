c2fd39368bcf246aa53e7a395f1e358f54488596
Eddie Taliaferro II <eddielacrosse2@gmail.com>
2018-02-24 02:52:37 -0500

    Update top5.component.ts
    Injected ArtistPageComponent into Top5Component to get each artist's artistName param value.

--- src/app/library/artists/artistpage/top5/top5.component.ts
+++ src/app/library/artists/artistpage/top5/top5.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute, Params, Router, ActivatedRouteSnapshot } from '@angular
 import { DataService } from './../../../../data.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { trigger, state, style, transition, animate, keyframes} from '@angular/animations';
-
+import { ArtistPageComponent } from './../artistpage.component';
 
 @Component({
   selector: 'top5',
@@ -17,13 +17,20 @@ export class Top5Component implements OnInit {
   albumsToDisplay: any = null;
   artistGenre: any = null;
   artistData = this.dataService.data.artists;
+  dataFromArtistPage: any = this.artistPageData;
+
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private dataService: DataService,
-    private  cdRef:ChangeDetectorRef
+    private  cdRef:ChangeDetectorRef,
 
+    // Have to inject ArtistPageComponent's data into the Top5Component
+    // Needs ArtistPageComponent's param.snapshot from the Artist's Name
+    // ArtistPageComponent = Parent Component
+    // Top5 = Child component
+    private artistPageData: ArtistPageComponent
   ){  }
 
   // Get all the albums in the entire database
@@ -35,14 +42,21 @@ export class Top5Component implements OnInit {
     return acc;
   }, []);
 
+  // This component's artistName property is equal to an
+  // object of an Artist in the dataService
   getArtistInfo(artistName: any) {
     return this.artistData.filter( artist => artist.artistName === artistName );
   }
 
   ngOnInit() {
-    this.artistName = this.route.snapshot.params['artistName'];
+    // gets the artistName of the ArtistPageComponent, which is
+    // a snapshot of its params in the route
+    this.artistName = this.dataFromArtistPage.artistName
     this.artistInfo = this.getArtistInfo(this.artistName);
-    console.log(this.route);
+    console.log(this.artistInfo);
+    console.log(" ^  -- This is all the artists' info in the DataService for the Top-5 Section")
+    console.log(this.dataFromArtistPage.artistName);
+    console.log( ' ^  -- This is the name of the artist from the ArtistPageComponent for the Top-5 Section')
   }
 
 }
