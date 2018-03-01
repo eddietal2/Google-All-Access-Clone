9688b4b9b93ae758eaf23452e9c3833784fd2b15
Eddie Taliaferro II <eddielacrosse2@gmail.com>
2018-03-01 02:08:38 -0500

    Update all-songs.component.ts

--- src/app/library/artists/artistpage/all-songs/all-songs.component.ts
+++ src/app/library/artists/artistpage/all-songs/all-songs.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from './../../../../data.service';
+import { SharedService } from './../../../../shared.service';
+import { ArtistPageComponent } from './../artistpage.component';
+
 
 @Component({
   selector: 'allsongs',
@@ -7,9 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllSongsComponent implements OnInit {
 
-  constructor() { }
+  artistData: any = this.dataService.data.artists;
+  dataFromArtistPage: any = this.artistPageData;
+  artistName: any = null;
+  artistsSongs: any = null;
+
+  constructor(
+    private dataService: DataService,
+    private sharedService: SharedService,
+    private artistPageData: ArtistPageComponent
+  ) { }
 
   ngOnInit() {
+    this.artistName = this.dataFromArtistPage.artistName;
+    this.artistsSongs = this.getSongs.filter( artist => artist.artistName === this.artistName);
+    console.log( this.artistsSongs)
   }
+
+  // Gets the object for each song that is selected
+  getSong(x){
+    console.log(x);
+    this.sharedService.emitChange(x);
+  }
+
+  getAlbums = this.artistData.reduce(
+    (acc: any, next: any) => {
+      next.albums.forEach(album => {
+        acc.push(album);
+      });
+    return acc;
+  }, []);
+
+  getSongs = this.getAlbums.reduce(
+    (acc: any, next: any) => {
+      next.songs.forEach(song => {
+        acc.push(song);
+      });
+    return acc;
+  }, []);
 
 }
