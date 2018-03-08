9a98e19c548b8f40952d810e1dba282a4681874f
Eddie Taliaferro II <eddielacrosse2@gmail.com>
2018-03-08 01:27:12 -0500

    Update related.component.ts
    added some functions to get the artist's genre and find other artists with the same genre. 
    Also added some Route stuffs.

--- src/app/library/artists/artistpage/related/related.component.ts
+++ src/app/library/artists/artistpage/related/related.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router, ActivatedRouteSnapshot } from '@angular/router';
+import { DataService } from './../../../../data.service';
 
 @Component({
   selector: 'related',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RelatedComponent implements OnInit {
 
-  constructor() { }
+  header = "Related Artists";
+  artistName: any;
+  artistData = this.dataService.data.artists;
+  artistInfo: any;
+  artistGenre: string;
+  relatedArtists: any;
+
+  constructor(
+    private dataService: DataService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  getArtistInfo(artistName: any) {
+    return this.artistData.filter( artist => artist.artistName === artistName );
+  }
+
+  getRelatedArtists(artistGenre: string) {
+    return this.artistData.filter( artist => artist.genre === artistGenre)
+  }
+
+  // Goes to this artists page
+  goToDetailPage(x){
+    this.router.navigate(['artists', x.artistName ])
+  }
+
 
   ngOnInit() {
+    this.artistName = this.route.snapshot.params['artistName'];
+    this.artistInfo = this.getArtistInfo(this.artistName);
+    this.artistGenre = this.artistInfo[0].genre;
+    this.relatedArtists = this.getRelatedArtists(this.artistGenre)
+    console.log(this.relatedArtists)
+    console.log(this.artistGenre)
   }
 
 }
